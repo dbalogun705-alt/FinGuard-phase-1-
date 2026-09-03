726a603fc4eebab2bc2f6e1602c3d5407ce0c346
Oluwadan26 <danieloluwaseun2009@gmail.com>
2026-09-03 21:19:44 +0100

    Changes made

--- javascript/risk explanation.js
+++ javascript/risk explanation.js
@@ -2,22 +2,3 @@
 // FINGUARD - RISK EXPLANATION
 // =====================================
 
-// Recommendation button
-const recommendationBtn =
-    document.getElementById("recommendationBtn");
-
-recommendationBtn.addEventListener("click", function () {
-
-    alert(
-        "Here you can view recommendations to improve your financial health."
-    );
-
-});
-// Sidebar
-window.addEventListener("message", function (event) {
-  if (event.source !== document.querySelector(".sidebar-frame").contentWindow) {
-    return;
-  }
-
-  document.body.classList.toggle("sidebar-collapsed", event.data.collapsed);
-});
